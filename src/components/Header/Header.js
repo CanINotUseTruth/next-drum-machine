8f6abe626ded3c22d05c269c7f423968c6c22cae
Truth <tobyrutherfordfb@gmail.com>
2024-10-02 22:39:09 +1000

    Complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,31 +4,25 @@ import { Volume2, VolumeX } from 'react-feather';
 
 import VisuallyHidden from '../VisuallyHidden';
 import MaxWidthWrapper from '../MaxWidthWrapper';
+import { useSoundEnabled } from '../SoundEnabledProvider/SoundEnabledProvider';
 import styles from './Header.module.css';
 
 function Header() {
   const id = React.useId();
 
-  // TODO: Global state?
-  const soundEnabled = true;
+  const { soundEnabled, setSoundEnabled } = useSoundEnabled();
 
   return (
     <header className={styles.wrapper}>
-      <MaxWidthWrapper
-        className={styles.innerWrapper}
-      >
+      <MaxWidthWrapper className={styles.innerWrapper}>
         <a href="/">Kool Website</a>
 
         <button
           onClick={() => {
-            // TODO: flip `soundEnabled`
+            setSoundEnabled(!soundEnabled);
           }}
         >
-          {soundEnabled ? (
-            <Volume2 />
-          ) : (
-            <VolumeX />
-          )}
+          {soundEnabled ? <Volume2 /> : <VolumeX />}
           <VisuallyHidden>
             {soundEnabled
               ? 'Disable sound effects'
